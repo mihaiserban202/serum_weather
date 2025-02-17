ad3dbfbbad4f8010fc83054d2c15167e6aa38811
laronichill <laronichill@gmail.com>
2025-02-17 16:59:24 -0500

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+
   final String title;
 
   @override
@@ -38,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+
       ),
       body:
     );
